aef6db53736b4d958fac736fe81aa0acce871c84
muratcanabay <muratcanabay@hotmail.com>
2019-12-13 08:36:42 +0300

    Initial commit

---
+++ src/main/java/com/uniyaz/javaOdev/SavePeople.java
@@ -0,0 +1,51 @@
+package com.uniyaz.javaOdev;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SavePeople implements Servlet {
+    List<String> personList = new ArrayList<>();
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        String tip = servletRequest.getParameter("tip");
+        System.out.println("Tipi: "+tip);
+        String ad = servletRequest.getParameter("ad");
+        String soyad = servletRequest.getParameter("soyad");
+
+        if (tip.equals("kaydet")&&(ad!=null && soyad!=null)){
+            System.out.println("sa");
+            personList.add(ad+" "+soyad);
+            System.out.println(ad+" "+soyad +" kaydi basariyla eklendi.");
+        }
+        else if (tip.equals("listele")&&personList.size()!=0){
+            for (int i = 0; i < personList.size(); i++) {
+                servletResponse.getWriter().write(personList.get(i) + "\n");
+                System.out.println("\n");
+            }
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
