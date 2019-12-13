f8e557e66e7e55babe7afe6d29993b7afd061206
muratcanabay <muratcanabay@hotmail.com>
2019-12-13 09:06:48 +0300

    UTF-8 eklendi.

--- src/main/java/com/uniyaz/javaOdev/SavePeople.java
+++ src/main/java/com/uniyaz/javaOdev/SavePeople.java
@@ -22,20 +22,18 @@ public class SavePeople implements Servlet {
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
 
         String tip = servletRequest.getParameter("tip");
-        System.out.println("Tipi: "+tip);
         String ad = servletRequest.getParameter("ad");
         String soyad = servletRequest.getParameter("soyad");
 
+        servletResponse.setCharacterEncoding("UTF-8");
+
         if (tip.equals("kaydet")&&(ad!=null && soyad!=null)){
-            System.out.println("sa");
             personList.add(ad+" "+soyad);
             System.out.println(ad+" "+soyad +" kaydi basariyla eklendi.");
         }
         else if (tip.equals("listele")&&personList.size()!=0){
             for (int i = 0; i < personList.size(); i++) {
-                servletResponse.setCharacterEncoding("UTF-8");
                 servletResponse.getWriter().write(personList.get(i) + "\n");
-                System.out.println("\n");
             }
         }
     }
